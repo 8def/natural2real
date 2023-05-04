255424d79f68409c90fbe9de342ccde88a7863e8
Thomas Palmer <143a.def@gmail.com>
2023-05-03 19:48:15 -0700

    Update real2Natural.js

--- real2Natural.js
+++ real2Natural.js
@@ -1,28 +1,14 @@
 // Form a bijection between the reals and naturals starting from the reals
 function real2Nat(r) {
-	// split the real number into a pair and a sign
+  // split the real number into a pair
   let p = getPair(r)
-  // map negatives to evens
-  if (r < 0)
-  	return negMap(p)
-  // map positives to odds  
-  if (p[0]+p[1]%2 !== 0)
-  	return posOddMap(p)
-    
-  return posEvenMap(p)
+  // map the pair to a natural
+  return szudzikPair(p)
 }
 
-// Cantor's pairing function for negatives
-function negMap(p) {
-  return 2*(p[0] + p[1])**2 + p[1];
-}
-// Cantor's pairing function for odd positives
-function posOddMap(p) {
-	return 2*(p[0] + p[1])**2 + p[0] + p[1] - 1
-}
-// Cantor's pairing function for even positives
-function posEvenMap(p) {
-	return 2*(p[0] + p[1])**2 + p[0] + p[1] + 1
+// Create a bijection between ordered pairs and naturals
+function szudzikPair(x, y){
+    return (x >= y ? (x * x) + x + y : (y * y) + x);
 }
 
 // seperate fractional/whole components from reals
